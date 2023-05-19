58a737f8a49c8996d4bac5b1e7bd2986412be1a5
nirkil7 <nirkil@bk.ru>
2023-05-19 17:25:43 +0700

    Calc2

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,28 @@
 import java.io.IOException;
-import java.util.*;
+import java.util.Scanner;
 class Main {
     public static String calc(String input) throws IOException {
-        String[] calc_inputString = input.split(" ");
+        Scanner inputString = new Scanner(System.in);
+        String input1 = inputString.nextLine();
+        String[] calc_inputString = input1.split(" ");
         if (calc_inputString.length != 3) {
             throw new IOException();
         }
-        return input;
+        return null;
     }
     public static void main(String[] args) throws IOException {
         Scanner inputString = new Scanner(System.in);
-        String input = inputString.nextLine();
-        String[] calc_inputString = input.split(" ");
-        int result = 0;
+        String input1 = inputString.nextLine();
+        String[] calc_inputString = input1.split(" ");
+        String operation = calc_inputString[1];
+        if (calc_inputString.length != 3) {
+            throw new IOException();
+        }
         int value1 = 0;
         int value2 = 0;
-        String operation = calc_inputString[1];
         value1 = Integer.parseInt(calc_inputString[0]);
         value2 = Integer.parseInt(calc_inputString[2]);
+        int result = 0;
         if (value1 < 1 | value1 > 10 | value2 < 1 | value2 > 10) {
             throw new IOException();
         }
